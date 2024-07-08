ce713ca64131d20a418eb48c66bfd5208b1585da
Saksham Rathi <sakshamrathi@Sakshams-MacBook-Pro.local>
2024-07-08 22:16:34 +0530

    added_files

--- 1983d.cpp
+++ 1983d.cpp
@@ -11,10 +11,6 @@ using namespace std;
 int _mergeSort(int arr[], int temp[], int left, int right);
 int merge(int arr[], int temp[], int left, int mid,
           int right);
-
-// This function sorts the
-// input array and returns the
-// number of inversions in the array
 int mergeSort(int arr[], int array_size)
 {
     int temp[array_size];
