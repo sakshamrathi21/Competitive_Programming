839f3a27b8b3ca31fb889c423f704ae68099391e
Saksham Rathi <sakshamrathi@Sakshams-MacBook-Pro.local>
2024-07-20 22:40:20 +0530

    added_files

---
+++ 1990c.cpp
@@ -0,0 +1,27 @@
+/**
+ *    Author: Saksham Rathi
+ *    Created: Sat Jul 20 21:33:27 IST 2024
+**/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0 ; i < n ; i ++) {
+            cin >> a[i];
+        }
+        int tot = 0;
+        vector<int> mad(n, 0);
+        
+    }
+}
