f20aa5e0521d1d0df914e0389b90aa41e23b4000
Saksham Rathi <sakshamrathi@Sakshams-MacBook-Pro.local>
2024-07-31 22:54:31 +0530

    Fixed_mse_graphs

--- m.sh
+++ m.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 file_name=$1
 
-# Export the file name as an environment variable to be used in the Makefile
 export file_name
 
-# Run make and redirect output to a file
 make > make_output.txt 2>&1
 
-# Check the return status of the make command
 if [ $? -ne 0 ]; then
     cat make_output.txt
     echo "Compilation failed"
@@ -15,7 +12,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# If make was successful, check for the specific "Nothing to be done for `all'" message
 if grep -q "Nothing to be done for \`all'" make_output.txt; then
     rm make_output.txt
 else
@@ -23,5 +19,4 @@ else
     rm make_output.txt
 fi
 
-# Run the compiled program
 ./practice
