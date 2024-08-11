3ab8c31746c7e2c2f589c27818561d0da7ac8889
Saksham Rathi <22b1003@iitb.ac.in>
2024-08-11 13:11:56 +0530

    added_files

--- trailing_zeros.cpp
+++ trailing_zeros.cpp
@@ -5,12 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     int pow = 0;
-
-    for (int i = 5; i <= n; i *= 5) {
-        pow += n / i;
-    }
-
+    for (int i = 5; i <= n; i *= 5) pow += n / i;
     cout << pow;
 }
