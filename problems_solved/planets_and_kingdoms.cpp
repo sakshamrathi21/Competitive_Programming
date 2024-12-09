1cb21b008e2ab76cb1bd30147839bf472ff653dc
Saksham Rathi <22b1003@iitb.ac.in>
2024-12-09 18:34:35 +0530

    scc

--- problems_solved/planets_and_kingdoms.cpp
+++ problems_solved/planets_and_kingdoms.cpp
@@ -1,38 +1,65 @@
+/**
+ *    Author: Saksham Rathi
+ *    Created: Mon Dec  9 17:39:27 IST 2024
+**/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+// const int MOD = 1000000007;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
 
-int n, m;
+struct SCC {
+	int N;
+	vector<vector<int>> adj, radj;
+	vector<int> todo, comp, comps;
+	vector<bool> vis;
+	void init(int _N) {
+		N = _N;
+		adj.resize(N), radj.resize(N), comp = vector<int>(N, -1), vis.resize(N);
+	}
+	void ae(int x, int y) { adj[x].push_back(y), radj[y].push_back(x); }
+	void dfs(int x) {
+		vis[x] = 1;
+		for (int y : adj[x])
+			if (!vis[y]) dfs(y);
+		todo.push_back(x);
+	}
+	void dfs2(int x, int v) {
+		comp[x] = v;
+		for (int y : radj[x])
+			if (comp[y] == -1) dfs2(y, v);
+	}
+	void gen() {  // fills allComp
+		for (int i = 0; i < N; i++)
+			if (!vis[i]) dfs(i);
+		reverse(begin(todo), end(todo));
+		for (int x : todo)
+			if (comp[x] == -1) { dfs2(x, x), comps.push_back(x); }
+	}
+};
 
-vector<vector<int>> adj(1e5+1);
+signed main() {
 
-vector<bool> visited(1e5+1, false);
+	int n, m, a, b;
+	cin >> n >> m;
 
-vector<int> componentids(1e5+1, 0);
+	SCC graph;
+	graph.init(n);
+	while (m--) {
+		cin >> a >> b;
+		graph.ae(--a, --b);
+	}
+	graph.gen();
 
-void DFS(int val, int componentID) {
-    if (visited[val]) return;
-    visited[val] = true;
-    componentids[val] = componentID;
-    for (auto u : adj[val]) DFS(u, componentID);
-}
+	int ID[200000]{};
+	int ids = 0;
+	for (int i = 0; i < n; i++) {
+		if (!ID[graph.comp[i]]) { ID[graph.comp[i]] = ++ids; }
+	}
+	cout << ids << '\n';
+	for (int i = 0; i < n; i++) { cout << ID[graph.comp[i]] << " \n"[i == n - 1]; }
 
-signed main () {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n >> m;
-    while (m-- ) {
-        int x, y;
-        cin >> x >> y;
-        adj[x].push_back(y);
-    }
-    int componentID = 1;
-    for (int i = 1 ; i <= n ; i ++ ) {
-        if (!visited[i]) DFS(i, componentID);
-        componentID ++;
-    }
-
-    cout << componentID - 1 << endl;
-    for (int i = 1 ; i <= n ; i ++) cout << componentids[i] << " ";
 }
