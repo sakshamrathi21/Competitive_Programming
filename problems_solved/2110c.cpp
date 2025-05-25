0d5e6c7314adb185d67f76edc1a342a415af0a0a
Saksham Rathi <22b1003@iitb.ac.in>
2025-05-25 19:47:12 +0100

    2110

--- problems_solved/2110c.cpp
+++ problems_solved/2110c.cpp
@@ -13,10 +13,53 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n);
+    vector<int> d(n), l(n), r(n);
     for (int i = 0 ; i < n ; i ++) {
-        cin >> a[i];
+        cin >> d[i];
     }
+    for (int i = 0 ; i < n ; i ++)
+    {
+        cin >> l[i] >> r[i];
+    }
+    int left = 0;
+    vector<int> unknowns;
+    for (int i = 0 ; i < n ; i ++)
+    {
+        if (d[i] == -1)
+        {
+            unknowns.push_back(i);
+        }
+        else
+        {
+            left += d[i];
+        }
+        while (left < l[i])
+        {
+            if (unknowns.size() == 0)
+            {
+                cout << "-1\n";
+                return;
+            }
+            d[unknowns[unknowns.size() - 1]] = 1;
+            left++;
+            unknowns.pop_back();
+        }
+        while (left + unknowns.size() > r[i])
+        {
+            if (unknowns.size() == 0)
+            {
+                cout << "-1\n";
+                return;
+            }
+            d[unknowns[unknowns.size() - 1]] = 0;
+            unknowns.pop_back();
+        }
+    }
+    for (int i = 0 ; i < n ; i ++)
+    {
+        cout << max(d[i], 0LL) << " ";
+    }
+    cout << "\n";
 }
 
 signed main () {
