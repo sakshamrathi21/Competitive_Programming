2f694065b60283946304c6e6d62446de81ad3431
Saksham Rathi <22b1003@iitb.ac.in>
2025-06-08 16:23:05 +0100

    2111

--- problems_solved/2111c.cpp
+++ problems_solved/2111c.cpp
@@ -17,6 +17,21 @@ void solve()
     for (int i = 0 ; i < n ; i ++) {
         cin >> a[i];
     }
+    int min_cost = 1e15;
+    int left = 0;
+    for (int i = 1 ; i < n ; i ++) {
+        if (a[i] == a[i-1]) continue;
+        else {
+            min_cost = min(min_cost, left*a[left] + a[left]*(n-i));
+            // cout << left << " " << right << " " << min_cost << "\n";
+            left = i;
+        }
+    }
+    if (left < n) {
+        min_cost = min(min_cost, left*a[left]);
+    }
+    min_cost = min(min_cost, (n-1)*a[n-1]);
+    cout << min_cost << "\n";
 }
 
 signed main () {
