32d51e9436ceb8cc37d3598ff92a4f84653f5b57
Saksham Rathi <22b1003@iitb.ac.in>
2025-07-17 08:22:54 +0530

    cses

--- problems_solved/counting_towers.cpp
+++ problems_solved/counting_towers.cpp
@@ -1,35 +1,34 @@
+/**
+ *    Author: Saksham Rathi
+ *    Created: Wed Jul 16 21:16:42 IST 2025
+**/
+
+
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define int long long
+const int MOD = 1000000007;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
 
-int main () {
+signed main () {
+    (void)MOD;
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int num_test_cases;
-    cin >> num_test_cases;
-    int input_number;
-    vector<int> all_test_numbers(num_test_cases);
-    int maximum_number = 0;
-    for (int i = 0 ; i < num_test_cases ; i ++ ) {
-        cin >> input_number;
-        all_test_numbers[i] = input_number;
-        maximum_number = max(maximum_number, input_number);
+    int t;
+    cin >> t;
+    vii dp(1e6+10, vector<int>(2));
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    for (int i = 2 ; i < 1e6+10 ; i ++) {
+        dp[i][0] = 4*dp[i-1][0] + dp[i-1][1];
+        dp[i][1] = dp[i-1][0] + 2*dp[i-1][1];
+        dp[i][0] %= MOD;
+        dp[i][1] %= MOD;
     }
-    input_number = maximum_number;
-    vector<pair<ll, ll> > required_values(input_number + 1);
-    // cout << required_values.size() << input_number << endl;
-    required_values[0].first = 0;
-    required_values[0].second = 0;
-    required_values[1].first = 1;
-    required_values[1].second = 1;
-    for (int i = 2 ; i <= input_number ; i ++ ) {
-        required_values[i].first = (2*required_values[i-1].first + required_values[i-1].second)%(1000000007);
-        required_values[i].second = (required_values[i-1].first + 4*required_values[i-1].second)%(1000000007);
-        // cout << required_values[i].first << required_values[i].second << endl;
-    }
-
-    for (int i = 0 ; i < num_test_cases ; i ++ ) {
-        cout << (required_values[all_test_numbers[i]].first + required_values[all_test_numbers[i]].second)%(1000000007) << '\n';
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << (dp[n][0] + dp[n][1])%MOD << "\n";
     }
 }
