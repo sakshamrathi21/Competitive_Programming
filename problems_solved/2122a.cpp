c2eae852250f2b26af4a07a0a5abd1bcb789fc5b
Saksham Rathi <22b1003@iitb.ac.in>
2025-07-19 21:08:34 +0530

    2122

---
+++ problems_solved/2122a.cpp
@@ -0,0 +1,29 @@
+/**
+ *    Author: Saksham Rathi
+ *    Created: Sat Jul 19 20:52:40 IST 2025
+**/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD = 1000000007;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    if ( n == 1 || m == 1 ) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+signed main () {
+    (void)MOD;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
