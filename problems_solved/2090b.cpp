b3319cf5f6bdd2c043cd40ad5eec9fe48c4953e3
Saksham Rathi <22b1003@iitb.ac.in>
2025-08-16 23:59:04 +0530

    2090

--- problems_solved/2090b.cpp
+++ problems_solved/2090b.cpp
@@ -3,38 +3,53 @@
  *    Created: Sat Aug 16 23:36:58 IST 2025
 **/
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int MOD = 1000000007;
 
 void solve() {
     int n, m;
     cin >> n >> m;
     vector<string> a(n);
-    for (int i = 0 ; i < n ; i ++) {
-        cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    // Prefix sums for rows and columns
+    vector<vector<int>> rowSum(n, vector<int>(m+1, 0));
+    vector<vector<int>> colSum(n+1, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            rowSum[i][j+1] = rowSum[i][j] + (a[i][j] == '1');
+            colSum[i+1][j] = colSum[i][j] + (a[i][j] == '1');
+        }
     }
-    for (int i = n - 1 ; i >= 0 ; i --) {
-        for (int j = m - 1 ; j >= 0 ; j --) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             if (a[i][j] == '0') continue;
-            if (i == 0 || j == 0) continue;
-            if (a[i-1][j] == '1' || a[i][j-1] == '1') continue;
-            cout << "NO\n";
-            return;
+
+            if (i == 0 || j == 0) continue; // first row or column always ok
+
+            bool ok = false;
+
+            // Check leftwards: row i, from col 0..j
+            if (rowSum[i][j+1] == j+1) ok = true;
+
+            // Check upwards: column j, from row 0..i
+            if (colSum[i+1][j] == i+1) ok = true;
+
+            if (!ok) {
+                cout << "NO\n";
+                return;
+            }
         }
     }
     cout << "YES\n";
 }
 
-signed main () {
-    (void)MOD;
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
     cin >> t;
-    while (t--) {
-        solve();
-    }
+    while (t--) solve();
 }
