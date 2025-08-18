f0d7dc2919d68f19a62ae7fd3f21cb5ecd960842
Saksham Rathi <22b1003@iitb.ac.in>
2025-08-18 23:08:27 +0530

    1986

--- problems_solved/1986e.cpp
+++ problems_solved/1986e.cpp
@@ -1,84 +1,69 @@
 /**
  *    Author: Saksham Rathi
- *    Created: Sun Jun 23 20:54:57 IST 2024
+ *    Created: Mon Aug 18 22:10:19 IST 2025
 **/
 
 
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+const int MOD = 1000000007;
+
+int vecs(vector<int>&x, int k) {
+    int n = x.size();
+    if (n == 0 || n == 1) return 0;
+    vector<int> lsum(n, 0), rsum(n, 0);
+    lsum[1] = (x[1] - x[0])/k;
+    for (int i = 2 ; i < n - 1; i += 2) {
+        lsum[i+1] = (x[i+1] - x[i])/k + lsum[i-1];
+    }
+    
+    rsum[n-2] = (x[n-1]-x[n-2])/k;
+    for (int i = n - 3 ; i >= 1 ; i -= 2) {
+        rsum[i-1] = (x[i] - x[i-1])/k + rsum[i+1];
+    }
+    int m = rsum[1];
+    for (int i = 2 ; i < n - 1 ; i += 2) {
+        m = min(m, lsum[i-1] + rsum[i+1]);
+    }
+    m = min(m, lsum[n-2]);
+    return m;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0 ; i < n ; i ++) {
+        cin >> a[i];
+    }
+    map<int, vector<int>> f;
+    for (int i = 0 ; i < n ; i ++) f[a[i] % k].push_back(a[i]);
+    int no = 0;
+    for (auto& e : f) {sort(e.second.begin(), e.second.end()); if (e.second.size() % 2) no++;}
+    if (no > 1) {
+        cout << "-1\n";
+        return;
+    }
+    int sum = 0;
+    for (auto& e : f) {
+        if (e.second.size() % 2) {
+            sum += vecs(e.second, k);
+        }
+        else {
+            for (int i = 0 ; i < e.second.size() ; i += 2) sum += (e.second[i+1] - e.second[i])/k;
+        }
+    }
+    cout << sum << "\n";
+}
 
 signed main () {
+    (void)MOD;
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
     cin >> t;
     while (t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> a(n);
-        for (int i = 0 ; i < n ; i ++) {
-            cin >> a[i];
-        }
-        // map<int, int> cnt;
-        map<int, vector<int>> modk;
-        for (int i = 0 ; i < n ; i ++) {
-            if (a[i]%k == 0) modk[k].push_back(a[i]);
-            else modk[a[i]%k].push_back(a[i]);
-        }
-        int odd = -1;
-        bool poss = true;
-        int tot = 0;
-        for (auto &e: modk) {
-            
-            if (e.second.size()%2 == 1) {
-                if (odd == -1) odd = e.first;
-                else {
-                    poss = false;
-                    break;
-                }
-            }
-            sort(e.second.begin(), e.second.end());
-            if (e.second.size() == 1) continue;
-            int cnt = 0;
-            if (e.second.size()%2 == 1) {
-                vector<int> odd_sum(e.second.size(), 0);
-                vector<int> even_sum(e.second.size(), 0);
-                even_sum[1] = (e.second[1]-e.second[0])/e.first;
-                for (int i = 2 ; i+1 < e.second.size() ; i += 2) {
-                    even_sum[i] = even_sum[i-1];
-                    even_sum[i+1] = even_sum[i] + (e.second[i+1]-e.second[i])/e.first;
-                }
-                for (int i = 1 ; i+1 < e.second.size() ; i+= 2) {
-                    odd_sum[i] = odd_sum[i-1];
-                    odd_sum[i+1] = odd_sum[i] + (e.second[i+1]-e.second[i])/e.first;
-                }
-                cnt = LLONG_MAX;
-                for (int i = 1 ; i < e.second.size() - 1; i ++) {
-                    if (i%2) {
-                        cnt = min(cnt, even_sum[i-1]+odd_sum[e.second.size()-1]-odd_sum[i]+e.second[i+1]-e.second[i-1]);
-                    }
-                    else {
-                        cnt = min(cnt, even_sum[i-1]+odd_sum[e.second.size()-1]-odd_sum[i]);
-                    }
-                    cnt = min(cnt, even_sum[e.second.size()-2]);
-                    cnt = min(cnt, odd_sum[e.second.size()-1]);
-                }
-            }
-            else {
-                for (int i = 0 ; i+1 < e.second.size() ; i += 2) {
-                    // cout << "hello" << e.first << endl;
-                    cnt += (e.second[i+1]-e.second[i])/e.first;
-                }
-            }
-            tot += cnt;
-        }
-        if (poss) {
-            cout << tot << "\n";
-        }
-        else {
-            cout << "-1\n";
-        }
-
+        solve();
     }
 }
